ac461bb8fe01c05bb7d00a5d84142c6c50dde22b
Anjana Madushanka <71401231+Anjana-Ma@users.noreply.github.com>
2021-01-29 00:29:33 +0530

    config.py

---
+++ tobrot/config.py
@@ -0,0 +1,19 @@
+from tobrot.sample_config import Config
+
+#read readme too before filling these stuffs
+
+class Config(Config):
+    TG_BOT_TOKEN= "1521484417:AAEHFILFqvEYAO8A-GiVA02PntFccYghgVI" #imp
+    APP_ID = 2710778 #imp
+    API_HASH = "a3e5503b94be442badc605ca7905c83c" #imp
+    AUTH_CHANNEL = [-1001283185883, 1468099476] #imp replace your_id with your id from telegram or delete
+    INDEX_LINK = "https://leech.art-of-tech.workers.dev
+    GLEECH_COMMAND = "gleech"
+    YTDL_COMMAND = 'ytdl2'
+    TELEGRAM_LEECH_COMMAND_G = "tleech"
+    CLONE_COMMAND_G = "gclone"
+    PYTDL_COMMAND_G = "pytdl2"
+    DESTINATION_FOLDER = "TorrentLeech-Gdrive"
+    LEECH_COMMAND = "leech2"
+    RCLONE_CONFIG = """type = drive \nclient_id = 595329019144-17ommms2dt354ddsq7kf9abg8ommk3o4.apps.googleusercontent.com \nclient_secret = 1uy9jkw9KKDFUzSzrphv1tvq \nscope = drive \nroot_folder_id = 1jRz24hTH6kdOz_42zKsPPtu8fmT0l0Jv \ntoken = {"access_token":"ya29.a0AfH6SMD4TKTwinkqbhdxxrYuMZ8DUIgjmkeCBKGadtkGdNQH3aJ-E81evF3sFX27UtC6Vm_vC1SeMMfMfKzB68I5gma04MP5tNYhrEftu3x6fxF9XJbypwZI5q-mCTdG4ZCc4T_-eITYC3DMXLINrGIWHlpndJ0wwjwtC77CspI","token_type":"Bearer","refresh_token":"1//0g-nyTA6YFvAdCgYIARAAGBASNwF-L9IrR4gDYIapeqq-ptPei49YS4IHlmgnWb6mr5nvvaSww3JVafH6Kdt6L4qdNU1jOBl38hw","expiry":"2021-01-28T14:06:17.0615914+05:30"}"""
+    #put your config(replacing new lines with \n) in triple quote like above: """<your one liner config>"""
