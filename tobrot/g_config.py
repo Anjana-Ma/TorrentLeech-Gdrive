66a4fb890c3050d2c809b945e2b5184c2ad4eb52
Anjana Madushanka <71401231+Anjana-Ma@users.noreply.github.com>
2021-01-29 00:28:32 +0530

    credits

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -5,21 +5,20 @@ from tobrot.sample_config import Config
 #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
 
 class Config(Config):
-    TG_BOT_TOKEN= "1268fill---your----data"
-    APP_ID = 1373938
-    API_HASH = "fill--your--data"
-    OWNER_ID = 12537936
-    AUTH_CHANNEL = [-10082786282972]
+    TG_BOT_TOKEN= "1521484417:AAEHFILFqvEYAO8A-GiVA02PntFccYghgVI"
+    APP_ID = 2710778
+    API_HASH = "a3e5503b94be442badc605ca7905c83c"
+    OWNER_ID = 1468099476
+    AUTH_CHANNEL = [-1001283185883]
     DESTINATION_FOLDER = "TorrentLeech-Gdrive" #Name of your folder read readme(not id of the folder)
     #Just don't fill RCLONE_CONFIG vars, insted copy your rclone.conf file in root directory
     #if your wanted to fill -- fill your rclone config like this(Your config may have some extra value or less. so Don't worry)
     RCLONE_CONFIG = """
-[DRIVE]
+[Torrent Leech]
 type = drive
-client_id = 28dslkgjsdl-fill-your-details-apps.googleusercontent.com
-client_secret = 6Tib48-fill-your-details-KuXXDX-eWgnRBYc
+client_id = 595329019144-17ommms2dt354ddsq7kf9abg8ommk3o4.apps.googleusercontent.com
+client_secret = 1uy9jkw9KKDFUzSzrphv1tvq
 scope = drive
-root_folder_id =
-token = {"access_token":"ya29.a-fill-your-details-af4ychuHswBt8X0nf2oWmczsHg6MYPSE6hXo-PJ-fill-your-details-s06KAecfw_H-tYThBtbs:20:25.430920315Z"}
-team_drive = 0AB0q-fill-your-details-sdrgsgsdUk9PVA
+root_folder_id = 1jRz24hTH6kdOz_42zKsPPtu8fmT0l0Jv
+token = {"access_token":"ya29.a0AfH6SMD4TKTwinkqbhdxxrYuMZ8DUIgjmkeCBKGadtkGdNQH3aJ-E81evF3sFX27UtC6Vm_vC1SeMMfMfKzB68I5gma04MP5tNYhrEftu3x6fxF9XJbypwZI5q-mCTdG4ZCc4T_-eITYC3DMXLINrGIWHlpndJ0wwjwtC77CspI","token_type":"Bearer","refresh_token":"1//0g-nyTA6YFvAdCgYIARAAGBASNwF-L9IrR4gDYIapeqq-ptPei49YS4IHlmgnWb6mr5nvvaSww3JVafH6Kdt6L4qdNU1jOBl38hw","expiry":"2021-01-28T14:06:17.0615914+05:30"}
 """
